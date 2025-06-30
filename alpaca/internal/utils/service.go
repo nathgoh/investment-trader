ffbb1f6b3b043227c5b5377ff62ec6add20290dc
Nathaniel Goh <gohnathaniel2000@gmail.com>
2025-06-29 18:26:50 -0700

    Change pkg to internal

--- alpaca/internal/utils/service.go
+++ alpaca/internal/utils/service.go
@@ -8,7 +8,7 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func GetAccount() *alpaca.Account {
+func GetPaperAccount() *alpaca.Account {
 	// Load .env file
 	err := godotenv.Load("../.env")
 	if err != nil {
@@ -27,6 +27,34 @@ func GetAccount() *alpaca.Account {
 		APIKey:    apiKey,
 		APISecret: apiSecret,
 		BaseURL:   "https://paper-api.alpaca.markets",
+	})
+	account, err := client.GetAccount()
+	if err != nil {
+		panic(err)
+	}
+
+	return account
+}
+
+func GetLiveAccount() *alpaca.Account {
+	// Load .env file
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Get API keys from environment variables
+	apiKey := os.Getenv("ALPACA_LIVE_API_KEY")
+	apiSecret := os.Getenv("ALPACA_LIVE_API_SECRET_KEY")
+
+	if apiKey == "" || apiSecret == "" {
+		log.Fatal("API key or secret not found in .env file")
+	}
+
+	client := alpaca.NewClient(alpaca.ClientOpts{
+		APIKey:    apiKey,
+		APISecret: apiSecret,
+		BaseURL:   "https://api.alpaca.markets",
 	})
 	account, err := client.GetAccount()
 	if err != nil {
