b07c5aa9c3c9941927afa95b38353a4f04656add
Nathaniel Goh <gohnathaniel2000@gmail.com>
2025-07-29 00:04:45 -0700

    Some alpaca slop

--- alpaca/cmd/main.go
+++ alpaca/cmd/main.go
@@ -3,35 +3,41 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
 
-	"github.com/alpacahq/alpaca-trade-api-go/v3/alpaca"
 	"github.com/joho/godotenv"
+
+	"github.com/nathgoh/investment-trader/alpaca/internal/marketdata"
 )
 
 func main() {
 	// Load .env file
-	err := godotenv.Load("../.env")
+	err := godotenv.Load("../../.env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
 	// Get API keys from environment variables
-	apiKey := os.Getenv("ALPACA_PAPER_API_KEY")
-	apiSecret := os.Getenv("ALPACA_PAPER_SECRET_KEY")
+	// apiKey := os.Getenv("ALPACA_PAPER_API_KEY")
+	// apiSecret := os.Getenv("ALPACA_PAPER_SECRET_KEY")
 
-	if apiKey == "" || apiSecret == "" {
-		log.Fatal("API key or secret not found in .env file")
-	}
+	// if apiKey == "" || apiSecret == "" {
+	// 	log.Fatal("API key or secret not found in .env file")
+	// }
 
-	client := alpaca.NewClient(alpaca.ClientOpts{
-		APIKey:    apiKey,
-		APISecret: apiSecret,
-		BaseURL:   "https://paper-api.alpaca.markets",
-	})
-	acct, err := client.GetAccount()
+	// client := alpaca.NewClient(alpaca.ClientOpts{
+	// 	APIKey:    apiKey,
+	// 	APISecret: apiSecret,
+	// 	BaseURL:   "https://paper-api.alpaca.markets",
+	// })
+	// acct, err := client.GetAccount()
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Printf("%+v\n", *acct)
+
+	quotes, err := marketdata.GetStockQuote("AAPL", 1, "7/25/2025")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%+v\n", *acct)
+	fmt.Printf("%+v\n", quotes)
 }
